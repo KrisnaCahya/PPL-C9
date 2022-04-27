b2474fe0d520f8fc7d937a0d5c65fcc275e0dc59
Fikri Ariqsya <fikriariqsyarahman98@gmail.com>
2022-04-28 04:14:22 +0700

    add database kripskuy latest version

--- kripskuy.sql
+++ kripskuy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2022 at 12:05 PM
+-- Generation Time: Apr 27, 2022 at 11:11 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -58,7 +58,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2022_04_10_201118_create_products_table', 1);
+(5, '2022_04_10_201118_create_products_table', 1),
+(6, '2022_04_27_184852_create_transaksis_table', 1);
 
 -- --------------------------------------------------------
 
@@ -98,12 +99,46 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `products` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `product_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` int(11) NOT NULL,
-  `stock` int(11) NOT NULL,
+  `nama_produk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `satuan` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah_produk_masuk` int(11) NOT NULL,
+  `jumlah_produk_keluar` int(11) NOT NULL,
+  `jumlah_sisa_produk` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `nama_produk`, `satuan`, `tanggal`, `jumlah_produk_masuk`, `jumlah_produk_keluar`, `jumlah_sisa_produk`, `created_at`, `updated_at`) VALUES
+(1, 'kripik singkong kocak aja', 21, '2022-04-09', 21, 11, 11, '2022-04-27 12:25:03', '2022-04-27 12:25:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksis`
+--
+
+CREATE TABLE `transaksis` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_produk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah_produk` int(11) NOT NULL,
+  `harga_satuan` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `transaksis`
+--
+
+INSERT INTO `transaksis` (`id`, `tanggal`, `nama_produk`, `jumlah_produk`, `harga_satuan`, `total_harga`, `created_at`, `updated_at`) VALUES
+(2, '2022-04-07', 'kripik pisang kocak geming', 1221, 1231, 211, '2022-04-27 12:17:55', '2022-04-27 12:18:33');
 
 -- --------------------------------------------------------
 
@@ -130,8 +165,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `nama`, `nohp`, `email`, `alamat`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'fkrsyy', 'Fikri Ariqsya', '081364739191', 'fikriariqsyarahman98@gmail.com', 'Bekasi', '$2y$10$rEiCOuWSUt5lQDMsTiSDxe8qmUx9dAAy4lu7Mx9jHgXC73fxaTFeK', 'mitra', NULL, '2022-04-18 01:18:40', '2022-04-18 01:18:40'),
-(2, 'pegawai1', 'pegawai 1', '081278459432', 'pegawai1@gmail.com', 'Bogor', '$2y$10$545um4iTRDOKuLBOw2BcMOJNxcpnJq0DyQ34CMZYsKdeKT/zde2ui', 'pegawai', NULL, '2022-04-18 01:26:16', '2022-04-18 01:26:16');
+(1, 'fkrsyy', 'Fikri Ariqsya', '081364739191', 'fikriariqsyarahman98@gmail.com', 'Bekasi', '$2y$10$itCj0r1JOc2d6atT0ftD4OurjparHfVK94kEHIOL4f3YaOdvNRs7m', 'owner', NULL, '2022-04-27 11:58:22', '2022-04-27 11:58:22'),
+(2, 'pegawai1', 'pegawai 1', '081234567890', 'Jon.Stephens@sakilastaff.com', 'Jakarta', '$2y$10$ZIH2R3gCzNf2AaDGOA3upOqa3U1yOXfX5qdgrGvanRYO05goXBGS6', 'pegawai', NULL, '2022-04-27 12:24:35', '2022-04-27 12:24:35');
 
 --
 -- Indexes for dumped tables
@@ -171,6 +206,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transaksis`
+--
+ALTER TABLE `transaksis`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -191,7 +232,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -203,7 +244,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `transaksis`
+--
+ALTER TABLE `transaksis`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
